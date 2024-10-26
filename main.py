1d8d94a0df963a75c0a0f8ed5d9ab360a694794b
NickTok64 <bignick5515@gmail.com>
2024-10-26 22:38:03 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+# 1st program
+print(5 * (9 ** 0.5))
+# 2nd program
+print(9.99 > 9.98 and 1000 != 1000.1)
+# 3nd program
+print(2 * 2 + 2)
+print(2 * (2 + 2))
+print((2 * 2 + 2) == (2 * (2 + 2)))
+# 4th program
+print(int((float('123.456') - float('123.456')//1) * 10))
